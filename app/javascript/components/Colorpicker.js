ccb7326b15ce6bdbaa6186edd2219b516def5efe
VirAlex <alexou73@gmail.com>
2020-01-07 18:36:26 +0100

    init color picker

---
+++ app/javascript/components/Colorpicker.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+class Colorpicker extends React.Component{
+  render() {
+    return  (
+            <React.Fragment>
+              <div>Hello</div>
+            </React.Fragment>)
+  }
+}
+
+Colorpicker.propTypes = {};
+
+export default Colorpicker
