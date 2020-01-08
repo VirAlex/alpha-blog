94199900aa707717705f4ec13010fb8c358ebc49
VirAlex <alexou73@gmail.com>
2020-01-08 10:50:05 +0100

    color prefered

--- app/javascript/components/Colorpicker.js
+++ app/javascript/components/Colorpicker.js
@@ -4,11 +4,26 @@ import PropTypes from "prop-types"
 import { SketchPicker} from "react-color"
 
 class Colorpicker extends React.Component{
+
+    constructor(props) {
+      super(props);
+      this.state = { selector: props.selector };
+      this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (color) => {
+    document.body.style.background = color.hex;
+    let elem = document.querySelector('#user_color')
+    elem.value = color.hex;
+  }
+
   render() {
     return  (
             <React.Fragment>
-              <SketchPicker />
-            </React.Fragment>)
+              <SketchPicker color={this.props.color}
+                            onChange={this.handleChange}/>
+            </React.Fragment>
+            );
   }
 }
 
